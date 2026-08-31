a4945a762fdc20decd0433ad0443ba9b40d0a947
promethee <dechateau.francois@gmail.com>
2026-08-31 17:43:53 +0200

    fix: hide native electron menu bar to avoid duplication

--- src/main/main.js
+++ src/main/main.js
@@ -5,6 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, '../preload/preload.js'),
       nodeIntegration: false,
