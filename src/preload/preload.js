632abcd1c684751a26ea9ef930932a57340bd917
promethee <dechateau.francois@gmail.com>
2026-09-02 17:56:40 +0200

    reset

--- src/preload/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile'),
-  saveFile: (content) => ipcRenderer.invoke('dialog:saveFile', content),
-  saveFileAs: (content) => ipcRenderer.invoke('dialog:saveFileAs', content),
-  readFile: (path) => ipcRenderer.invoke('file:read', path),
-  writeFile: ({ filePath, content }) => ipcRenderer.invoke('file:write', { filePath, content }),
-});
